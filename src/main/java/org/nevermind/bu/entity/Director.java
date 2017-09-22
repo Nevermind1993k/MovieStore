78ec8dab71661d47fa2e15992b17a2c886bc62f3
Roman <nevermind1993k@gmail.com>
2017-09-22 22:56:51 +0300

    Movie refactor and Director implementation

--- src/main/java/org/nevermind/bu/entity/Director.java
+++ src/main/java/org/nevermind/bu/entity/Director.java
@@ -3,8 +3,53 @@ package org.nevermind.bu.entity;
 import java.sql.Date;
 
 public class Director {
-    int id;
-    String name;
-    Date dateofBirth;
-    int movieId;
+    private int id;
+    private String name;
+    private Date dateofBirth;
+    private int movieId;
+
+    public Director() {
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Date getDateofBirth() {
+        return dateofBirth;
+    }
+
+    public void setDateofBirth(Date dateofBirth) {
+        this.dateofBirth = dateofBirth;
+    }
+
+    public int getMovieId() {
+        return movieId;
+    }
+
+    public void setMovieId(int movieId) {
+        this.movieId = movieId;
+    }
+
+    @Override
+    public String toString() {
+        return "Director{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", dateofBirth=" + dateofBirth +
+                ", movieId=" + movieId +
+                '}';
+    }
 }
