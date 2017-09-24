7592958260c2f3f2094c2b9ae92a8421a6756850
Roman <nevermind1993k@gmail.com>
2017-09-24 14:11:35 +0300

    MovieService and MovieServiceImpl added

---
+++ src/main/java/org/nevermind/bu/service/MovieService.java
@@ -0,0 +1,18 @@
+package org.nevermind.bu.service;
+
+import org.nevermind.bu.entity.Movie;
+
+import java.util.List;
+
+public interface MovieService {
+
+    Movie getById(int id);
+
+    List<Movie> getAll();
+
+    void save(Movie movie);
+
+    void delete(int id);
+
+    void update(Movie movie);
+}
---
+++ src/main/java/org/nevermind/bu/service/MovieServiceImpl.java
@@ -0,0 +1,38 @@
+package org.nevermind.bu.service;
+
+import org.nevermind.bu.dao.MovieDao;
+import org.nevermind.bu.entity.Movie;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.List;
+
+public class MovieServiceImpl implements MovieService {
+
+    @Autowired
+    MovieDao movieDao;
+
+    @Override
+    public Movie getById(int id) {
+        return movieDao.getMovieById(id);
+    }
+
+    @Override
+    public List<Movie> getAll() {
+        return movieDao.getAll();
+    }
+
+    @Override
+    public void save(Movie movie) {
+        movieDao.save(movie);
+    }
+
+    @Override
+    public void delete(int id) {
+        movieDao.delete(id);
+    }
+
+    @Override
+    public void update(Movie movie) {
+        movieDao.update(movie);
+    }
+}
